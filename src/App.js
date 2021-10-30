37e94f52c472e5fdd60935a978798c1802111c73
Shubhankar Thakur <shubhankarthakur75@.com>
2021-10-30 11:15:56 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import { Component } from "react";
-import FunComponent from './components/FunComponent';
-import ClassComponent from './components/ClassComponent';
+import FunComponent from "./components/FunComponent";
+import ClassComponent from "./components/ClassComponent";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   state = {
     isClassVisible: false,
     isFunctionVisible: false,
-  }
+  };
 
-  displayFunction=()=>{
+  displayFunction = () => {
     if (this.state.isFunctionVisible == true) {
       this.setState({ isFunctionVisible: false });
-    }
-    else {
+    } else {
       this.setState({ isFunctionVisible: true });
     }
-  }
+  };
 
-  displayClass=()=>{
+  displayClass = () => {
     if (this.state.isClassVisible == true) {
       this.setState({ isClassVisible: false });
-    }
-    else {
+    } else {
       this.setState({ isClassVisible: true });
     }
-  }
-  
+  };
 
-  render(){
+  render() {
     return (
       <div className="App">
         <div className="header">
+          <h1>Hello</h1>
+
           <h2>Styling Using Functional and Class Component</h2>
         </div>
         <div className="main-container">
           <div className="container1">
-
             <button onClick={this.displayFunction}>
               <h3>To see styling in functional component</h3>
             </button>
@@ -50,11 +48,19 @@ class App extends Component {
 
           <div className="container2">
             <div className="tabs">
-              <div  style={{display:this.state.isFunctionVisible? "block":"none"}}>
-                <FunComponent/>
+              <div
+                style={{
+                  display: this.state.isFunctionVisible ? "block" : "none",
+                }}
+              >
+                <FunComponent />
               </div>
-              <div style={{display:this.state.isClassVisible? "block":"none"}}>
-                <ClassComponent/>
+              <div
+                style={{
+                  display: this.state.isClassVisible ? "block" : "none",
+                }}
+              >
+                <ClassComponent />
               </div>
             </div>
           </div>
